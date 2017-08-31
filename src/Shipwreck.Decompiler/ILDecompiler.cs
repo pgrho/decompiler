5564614db4d71e5b99cb6e96a0b6a17a412abff5
pgrho <shipwreck@live.jp>
2017-08-31 11:44:01 +0900

    Use unsafe

--- src/Shipwreck.Decompiler/ILDecompiler.cs
+++ src/Shipwreck.Decompiler/ILDecompiler.cs
@@ -7,47 +7,50 @@ namespace Shipwreck.Decompiler
 {
     public static class ILDecompiler
     {
-        public static List<Syntax> Decompile(MethodBase method)
+        public static unsafe List<Syntax> Decompile(MethodBase method)
         {
             var bytes = method.GetMethodBody().GetILAsByteArray();
 
             var ret = new List<Syntax>(Math.Min(bytes.Length, Math.Max(4, bytes.Length >> 2)));
-
-            for (var i = 0; i < bytes.Length; i++)
+            fixed (byte* bp = bytes)
             {
-                var b = bytes[i];
-                switch (b)
+                for (var i = 0; i < bytes.Length; i++)
                 {
-                    case 0x00: // nop
-                        continue;
+                    var b = bytes[i];
+                    switch (b)
+                    {
+                        case 0x00: // nop
+                            continue;
 
-                    case 0x15: // ldc.i4.m1
-                    case 0x16: // ldc.i4.0
-                    case 0x17: // ldc.i4.1
-                    case 0x18: // ldc.i4.2
-                    case 0x19: // ldc.i4.3
-                    case 0x1A: // ldc.i4.4
-                    case 0x1B: // ldc.i4.5
-                    case 0x1C: // ldc.i4.6
-                    case 0x1D: // ldc.i4.7
-                    case 0x1e: // ldc.i4.8
-                        ret.Add(new LoadInt32Instruction(b - 0x16));
-                        break;
+                        case 0x15: // ldc.i4.m1
+                        case 0x16: // ldc.i4.0
+                        case 0x17: // ldc.i4.1
+                        case 0x18: // ldc.i4.2
+                        case 0x19: // ldc.i4.3
+                        case 0x1A: // ldc.i4.4
+                        case 0x1B: // ldc.i4.5
+                        case 0x1C: // ldc.i4.6
+                        case 0x1D: // ldc.i4.7
+                        case 0x1e: // ldc.i4.8
+                            ret.Add(new LoadInt32Instruction(b - 0x16));
+                            break;
 
-                    case 0x1f: //ldc.i4.s {num}
-                        ret.Add(new LoadInt32Instruction(bytes[++i]));
-                        break;
+                        case 0x1f: //ldc.i4.s {num}
+                            ret.Add(new LoadInt32Instruction(bytes[++i]));
+                            break;
 
-                    case 0x20: //ldc.i4 {num}
-                        ret.Add(new LoadInt32Instruction(bytes[++i] + (bytes[++i] << 8) + (bytes[++i] << 16) + (bytes[++i] << 24)));
-                        break;
+                        case 0x20: //ldc.i4 {num}
+                            ret.Add(new LoadInt32Instruction(*(int*)(bp + i + 1)));
+                            i += 4;
+                            break;
 
-                    case 0x2a: // ret
-                        ret.Add(new ReturnInstruction());
-                        break;
+                        case 0x2a: // ret
+                            ret.Add(new ReturnInstruction());
+                            break;
 
-                    default:
-                        throw new NotImplementedException();
+                        default:
+                            throw new NotImplementedException();
+                    }
                 }
             }
 
