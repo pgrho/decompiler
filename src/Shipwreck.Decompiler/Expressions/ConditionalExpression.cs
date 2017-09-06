541a5031611e15e443a958b5f342341ba2f127a4
pgrho <shipwreck@live.jp>
2017-09-06 16:46:12 +0900

    Added ConditionalExpression

---
+++ src/Shipwreck.Decompiler/Expressions/ConditionalExpression.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+
+namespace Shipwreck.Decompiler.Expressions
+{
+    public sealed class ConditionalExpression : Expression
+    {
+        internal ConditionalExpression(Expression condition, Expression truePart, Expression falsePart)
+        {
+            condition.ArgumentIsNotNull(nameof(condition));
+            truePart.ArgumentIsNotNull(nameof(truePart));
+            falsePart.ArgumentIsNotNull(nameof(falsePart));
+
+            Condition = condition;
+            TruePart = truePart;
+            FalsePart = falsePart;
+        }
+
+        public Expression Condition { get; }
+        public Expression TruePart { get; }
+        public Expression FalsePart { get; }
+
+        public override Type Type
+            => TruePart.Type.IsAssignableFrom(FalsePart.Type) ? TruePart.Type : FalsePart.Type;
+
+        public override bool IsEquivalentTo(Syntax other)
+            => this == (object)other
+                || (other is ConditionalExpression be
+                    && Condition.IsEquivalentTo(be.Condition)
+                    && TruePart.IsEquivalentTo(be.TruePart)
+                    && FalsePart.IsEquivalentTo(be.FalsePart));
+
+        public override void WriteTo(TextWriter writer)
+        {
+            writer.WriteSecondChild(Condition, this);
+            writer.Write(" ? ");
+            writer.WriteSecondChild(TruePart, this);
+            writer.Write(" : ");
+            writer.WriteFirstChild(FalsePart, this);
+        }
+
+        internal override Expression ReduceCore()
+        {
+            if (Condition.TryReduce(out var l) | TruePart.TryReduce(out var r) | FalsePart.TryReduce(out var f))
+            {
+                return new ConditionalExpression(l, r, f);
+            }
+
+            return this;
+
+            return base.ReduceCore();
+        }
+
+        public override ExpressionPrecedence Precedence
+            => ExpressionPrecedence.Conditional;
+    }
+}
