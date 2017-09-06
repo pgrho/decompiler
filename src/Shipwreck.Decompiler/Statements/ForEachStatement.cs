3d66e0510803d7df5d4a0957b48b54ce733bb907
pgrho <shipwreck@live.jp>
2017-09-06 17:39:34 +0900

    Added ForEachStatement

---
+++ src/Shipwreck.Decompiler/Statements/ForEachStatement.cs
@@ -0,0 +1,109 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using Shipwreck.Decompiler.Expressions;
+
+namespace Shipwreck.Decompiler.Statements
+{
+    public sealed class ForEachStatement : Statement
+    {
+        public Type Type { get; set; }
+
+        public string Identifier { get; set; }
+
+        public Expression Expression { get; set; }
+
+        #region Statements
+
+        private StatementCollection _Statements;
+
+        public StatementCollection Statements
+            => _Statements ?? (_Statements = new StatementCollection(this));
+
+        public bool ShouldSerializeStatements()
+            => _Statements.ShouldSerialize();
+
+        #endregion Statements
+
+        public override bool IsEquivalentTo(Syntax other)
+            => this == (object)other
+            || (other is ForEachStatement ws
+                && Type == ws.Type
+                && Identifier == ws.Identifier
+                && Expression.IsEquivalentTo(ws.Expression)
+                && _Statements.IsEquivalentTo(ws._Statements));
+
+        public override void WriteTo(IndentedTextWriter writer)
+        {
+            writer.Write("foreach (");
+
+            writer.Write(Type?.FullName ?? "var");
+            writer.Write(' ');
+
+            writer.Write(Identifier);
+
+            writer.Write(" in ");
+            Expression.WriteTo(writer);
+            writer.WriteLine(")");
+            writer.WriteLine('{');
+            if (ShouldSerializeStatements())
+            {
+                writer.Indent++;
+                foreach (var s in _Statements)
+                {
+                    s.WriteTo(writer);
+                }
+                writer.Indent--;
+            }
+            writer.WriteLine('}');
+        }
+
+        public override IEnumerable<StatementCollection> GetChildCollections()
+        {
+            if (ShouldSerializeStatements())
+            {
+                yield return _Statements;
+            }
+        }
+
+        public override bool Reduce()
+        {
+            var thisReduced = Expression.TryReduce(out var nc);
+            if (thisReduced)
+            {
+                Expression = nc;
+            }
+
+            if (Collection != null)
+            {
+                bool iterReduced;
+                do
+                {
+                    iterReduced = _Statements.ReduceBlock();
+                    thisReduced |= iterReduced;
+                }
+                while (iterReduced);
+
+                // TODO: Determine the Statements is empty
+            }
+
+            return thisReduced;
+        }
+
+        public override Statement Clone()
+        {
+            var r = new ForEachStatement()
+            {
+                Type = Type,
+                Identifier = Identifier,
+                Expression = Expression
+            };
+            if (ShouldSerializeStatements())
+            {
+                r.Statements.AddRange(_Statements.Select(s => s.Clone()));
+            }
+            return r;
+        }
+    }
+}
